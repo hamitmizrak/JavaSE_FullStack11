47d4865bfb88b03f6a83c05c5c1f4d06c9b3707f
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-09 12:15:15 +0300

    Examples

--- src/main/java/com/hamitmizrak/_04_Examples.java
+++ src/main/java/com/hamitmizrak/_04_Examples.java
@@ -1,45 +1,47 @@
 package com.hamitmizrak;
 
 public class _04_Examples {
+    public static void main(String[] args) {
 
-// 1.soru
+        // 1.soru
 // 1.dereceden 2 bilinmeyenli denklem
 // y=3x+4k;
 // x=5;
 // k=3
 
-int x = 5;
-int k = 3;
-int y = 3 * x + 4 * k;
-System.out.println(y);
+        int x = 5;
+        int k = 3;
+        int y = 3 * x + 4 * k;
+        System.out.println(y);
 
 // 2.soru
 // Dereceyi Fahrenhayt
 // (derece*9/5)+32
 // 0 derece 32 fahrenhayttır
-int degree = 0, fahranhayt;
-fahranhayt = (degree * 9 / 5) + 32;
-System.out.println(fahranhayt);
+        int degree = 0, fahranhayt;
+        fahranhayt = (degree * 9 / 5) + 32;
+        System.out.println(fahranhayt);
 
 
 // 3.soru iki sayıda dört işlem
 // x1=4,x2=2;
 // bölme/çıkarma/çarpma/toplama/bölümünden kalan(%)
-var x1 = 4;
-var x2 = 2;
-var toplam = x1 + x2;
-System.out.println(toplam);
+        var x1 = 4;
+        var x2 = 2;
+        var toplam = x1 + x2;
+        System.out.println(toplam);
 
-var cikarma = x1 - x2;
-System.out.println(cikarma);
+        var cikarma = x1 - x2;
+        System.out.println(cikarma);
 
-var carpma = x1 * x2;
-System.out.println(carpma);
+        var carpma = x1 * x2;
+        System.out.println(carpma);
 
-var bolme = x1 / x2;
-System.out.println(bolme);
+        var bolme = x1 / x2;
+        System.out.println(bolme);
 
-var kalan = x1 % x2;
-System.out.println(kalan);
+        var kalan = x1 % x2;
+        System.out.println(kalan);
 
+    }
 }
