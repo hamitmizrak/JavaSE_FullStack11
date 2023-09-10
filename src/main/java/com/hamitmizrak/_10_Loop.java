54460ca2151bcd2cb4c604e52adc202c3a9e8306
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-10 11:16:56 +0300

    Loop

--- src/main/java/com/hamitmizrak/_10_Loop.java
+++ src/main/java/com/hamitmizrak/_10_Loop.java
@@ -4,32 +4,27 @@ public class _10_Loop {
     public static void main(String[] args) {
         // Loop
         // break, return,continue
+        for (int i = 1; i <=9; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println("");
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // Date
-        // Exception Handling
-        // Access Modifier (public-private-default-protected)
-        // Class bileşenleri
-        // Override
-        // Overloading
-        // Encapsulation
-        // Method
-        // Primitive Method
-        // Recursive Method
-        // Enum
-        // Dizi
-        // Inner Class
-        // File
-        // Threading
-        // Collections
-        // Java 8 gelen özelliklerde
-        // OOP
-        // Class (Lombok)
-        // inheritance
-        // abstract
-        // interface
-        // polymorhism
-        // this, super
-        // new, null,zero
+        int k = 1;
+        while(k <=9){
+            System.out.print(k+" ");
+            // i++;
+            // i+=1;
+            k=k+1;
+        }
+
+        System.out.println("");
+
+        int w = 1;
+        do{
+            System.out.print(w+" ");
+            w+=1;
+        }
+        while(w <=9);
+
     }
 }
